610686b63368570407ee630bd9f2a6f4ddce9264
VK <vitaliy.krynytskyy@gmail.com>
2020-03-26 11:44:31 +0000

    Removed optional chaining and replaced with getInitialProps

--- pages/characters/[id].js
+++ pages/characters/[id].js
@@ -1,20 +1,34 @@
 import Layout from "../../components/Layout";
 
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 import characters from "../../data/characters.json";
 
-const Character = () => {
-  const router = useRouter();
-  const character = characters[router.query.id];
+const Character = ({ character }) => {
+  // const Character = () => {
+  //   const router = useRouter();
+  //   const character = characters[router.query.id];
 
   return (
+    // <Layout>
+    //   <h1>{character?.name}</h1>
+    //   <p>
+    //     {character?.name} is from {character?.homeworld}
+    //   </p>
+    // </Layout>
     <Layout>
-      <h1>{character?.name}</h1>
+      <h1>{character.name}</h1>
       <p>
-        {character?.name} is from {character?.homeworld}
+        {character.name} is from {character.homeworld}
       </p>
     </Layout>
   );
+};
+
+// without ssr
+Character.getInitialProps = ({ query }) => {
+  const { id } = query;
+  const character = characters[id];
+  return { character };
 };
 
 export default Character;
