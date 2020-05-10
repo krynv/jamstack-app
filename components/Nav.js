c46fcc97f328b06b7cf3c26ffbf5ef83b5e5b520
VK <vitaliy.krynytskyy@gmail.com>
2020-05-10 11:43:48 +0100

    Replaced Auth0 authentication with authentication from strapi

--- components/Nav.js
+++ components/Nav.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "./Link";
+import fetch from "isomorphic-unfetch";
 
 import { useUser } from "../lib/user";
+import { setToken, unsetToken } from "../lib/auth";
 
 const Nav = () => {
   const { user, loading } = useUser();
+  const [data, setData] = useState({
+    identifier: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch(`${process.env.API_BASE_URL}/auth/local`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        identifier: data.identifier,
+        password: data.password,
+      }),
+    });
+
+    const responseData = await response.json();
+
+    setToken(responseData);
+  };
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const logout = () => {
+    unsetToken();
+  };
+
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-primary fixed-top">
       <a className="navbar-brand" href="/">
-        SW App
+        Ster Wers App
       </a>
       <button
         className="navbar-toggler"
@@ -43,24 +77,47 @@ const Nav = () => {
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/api/logout">
+                  <a
+                    className="nav-link"
+                    onClick={logout}
+                    style={{ cursor: "pointer" }}
+                  >
                     Logout
                   </a>
                 </li>
               </>
             ) : (
-              <li className="nav-item right">
-                <a className="nav-link" href="/api/login">
-                  Login
-                </a>
-              </li>
+              ""
             ))}
         </ul>
+        {!loading && !user ? (
+          <form onSubmit={handleSubmit} className="form-inline">
+            <input
+              type="text"
+              name="identifier"
+              onChange={handleChange}
+              className="form-control mr-sm-2"
+              placeholder="Username"
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              className="form-control mr-sm-2"
+              placeholder="Password"
+              required
+            />
+            <button className="btn btn-success my-2 my-sm-0" type="submit">
+              Login
+            </button>
+          </form>
+        ) : (
+          ""
+        )}
         {!loading &&
           (user ? (
-            <span className="navbar-text">
-              Welcome back {user.name ? user.name : user.username}
-            </span>
+            <span className="navbar-text">Welcome back {user}</span>
           ) : (
             ""
           ))}
--- next.config.js
+++ next.config.js
@@ -2,18 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   env: {
-    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
-    REDIRECT_URI:
-      process.env.REDIRECT_URI || "http://localhost:3000/api/callback",
-    POST_LOGOUT_REDIRECT_URI:
-      process.env.POST_LOGOUT_REDIRECT_URI || "http://localhost:3000/",
-    SESSION_COOKIE_SECRET: process.env.SESSION_COOKIE_SECRET,
-    SESSION_COOKIE_LIFETIME: 7200,
-    API_AUDIENCE: "https://jamstackapp-api/",
-    AUTH0_SCOPE: "openid profile offline_access read:favourites",
-    API_BASE_URL: "http://localhost:4000",
+    API_BASE_URL: "http://localhost:1337",
     CLD_CLOUD_NAME: "dcgrlda2p",
   },
 };
